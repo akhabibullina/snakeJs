e4c4ddf8f7a681a69fc715f4eedf8167adfaed8d
akhabibullina <ascii88@mail.ru>
2012-12-02 15:26:21 +0300

    board class created

--- js/app.js
+++ js/app.js
@@ -1,18 +1,22 @@
 /**
- * We use requirejs since it limits the scope and thus prevents namespace conflicts.
+ * Use requirejs since it limits the scope and thus prevents namespace conflicts.
  */
 requirejs.config({
    baseUrl: "js",
    paths: {
-      "lib": "lib"
+      "lib": "lib",
+      "jquery": "lib/jquery.min"
    }
+//   shim: {
+//      'lib/jquery-ui/js/jquery-ui.min': {
+//         deps: ['jquery'],
+//         exports: 'jquery-ui'
+//      }
+//   }
 });
 
-define(['snake', 'board', 'food'], function() {
-
-   var mySnakeBoard = new Snake.Board({
-      boardContainer: "game-area",
-      fullScreen: true
-   });
+define(['board'], function(Board) {
+   var canvas = document.getElementById('game-area');
+   var mySnakeBoard = new Board({ "gamePlay" : canvas});
 
 });
--- js/board.js
+++ js/board.js
@@ -2,11 +2,39 @@
 * This class manages playing board for the game.
 * @class Board
 */
-/**
- * According to rules, board generates the food.
- * We also put responsibility that it should handle all the board staff.
- */
-Snake.Board = Snake.Board || (function() {
-    
-    // end return function
-})();
+define(['jquery'], function($) {
+
+    Board = function (inputData) {
+    var that = this;
+    if (inputData.gamePlay.getContext) {
+       this.drawBoard(inputData.gamePlay.getContext('2d'));
+    }
+    this.offsetX = inputData.gamePlay.offsetTop;
+    this.offsetY = inputData.gamePlay.offsetLeft;
+    this.width = inputData.gamePlay.width;
+    this.height = inputData.gamePlay.height;
+    // add listener to button
+    $('#start-fight').click(function(){
+       that.drawFood({'x': that.offsetX, 'y': that.offsetY,
+                      'width': that.width, 'height': that.height});
+    });
+    }
+
+    Board.prototype.drawBoard = function (gamePlayCtx) {
+      gamePlayCtx.fillRect(0,0,300,300);
+      gamePlayCtx.clearRect(10,10,280,130);
+    }
+
+    Board.prototype.drawFood = function (boardParams) {
+       var randomCoordinates = getRandomPosition(boardParams);
+       // draw food
+    }
+
+    function getRandomPosition(limitCoordinates) {
+       var x = Math.floor((Math.random()*(limitCoordinates.height+limitCoordinates.x))+limitCoordinates.x);
+       var y = Math.floor((Math.random()*(limitCoordinates.width+limitCoordinates.y))+limitCoordinates.y);
+       return {'x': x, 'y': y}
+    }
+    // and return an object
+    return Board;
+});
