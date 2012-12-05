b2e4b154a7d74fafa1aa3ada710cbdb0a362193f
akhabibullina <ascii88@mail.ru>
2012-12-05 19:19:11 +0200

    Add RaphaelJs with demo

--- js/app.js
+++ js/app.js
@@ -6,17 +6,26 @@ requirejs.config({
    paths: {
       "lib": "lib",
       "jquery": "lib/jquery.min"
+   },
+   shim: {
+      'lib/raphael': {
+         exports: 'Raphael'
+      }
    }
-//   shim: {
-//      'lib/jquery-ui/js/jquery-ui.min': {
-//         deps: ['jquery'],
-//         exports: 'jquery-ui'
-//      }
-//   }
 });
 
 define(['board'], function(Board) {
    var canvas = document.getElementById('game-area');
    var mySnakeBoard = new Board({ "gamePlay" : canvas});
 
+//// Creates canvas 320 × 200 at 10, 50
+//var paper = Raphael(10, 50, 320, 200);
+//
+//// Creates circle at x = 50, y = 40, with radius 10
+//var circle = paper.circle(50, 40, 10);
+//// Sets the fill attribute of the circle to red (#f00)
+//circle.attr("fill", "#f00");
+//
+//// Sets the stroke attribute of the circle to white
+//circle.attr("stroke", "#fff");
 });
